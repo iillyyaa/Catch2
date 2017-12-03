a12b01d6b6b6bdf82eb006ccbdcf8337212bb7a0
Martin Hořeňovský <martin.horenovsky@gmail.com>
2017-12-03 11:28:19 +0100

    Move scripts to new location

--- misc/appveyorTestRunScript.bat
+++ misc/appveyorTestRunScript.bat
@@ -2,7 +2,7 @@ cd Build
 if "%CONFIGURATION%"=="Debug" (
   echo "appveyorTestScript.bat thinks this is a Debug build"
   ctest -j 2 -C %CONFIGURATION% -D ExperimentalMemCheck
-  python ..\scripts\appveyorMergeCoverageScript.py
+  python ..\misc\appveyorMergeCoverageScript.py
   codecov --root .. --no-color --disable gcov -f cobertura.xml -t %CODECOV_TOKEN%
 )
 if "%CONFIGURATION%"=="Release" (
